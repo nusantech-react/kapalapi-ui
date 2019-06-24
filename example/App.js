6ea3cc85d1eb9e182b61872108dae7342b51e03f
Muhammad Hanif <hanifeij@yahoo.co.id>
2019-06-24 16:49:19 +0700

    initial commit

--- example/App.js
+++ example/App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Avatar} from 'kapalapi-ui'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-export * as Avatar from './src/components/Avatar';
-export * as Image from './src/components/Image';
+export { Avatar } from './src/components/Avatar';
+export { Image } from './src/components/Image';
